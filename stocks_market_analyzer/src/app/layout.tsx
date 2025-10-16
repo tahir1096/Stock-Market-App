03fc06ead043061df8293d49ca2d5ecebc9b99af
Muhammad Tahir <mtahirkaam@gmail.com>
2025-10-16 16:17:23 +0500

    Add global TypeScript definitions for user authentication, stock data, and alerts

--- stocks_market_analyzer/src/app/layout.tsx
+++ stocks_market_analyzer/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -14,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Signalist",
-  description: "Real-Time Stock Analyzer & Advanced Charting Tools",
+  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
 };
 
 export default function RootLayout({
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
